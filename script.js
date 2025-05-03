962618990c39879d994adf148ee3fc4c0d24e179
Akihiko <115684398+Akihiko2004@users.noreply.github.com>
2025-05-03 20:48:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const heroSection = document.getElementById('heroSection');
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
-  const heroHeight = heroSection.offsetHeight;
-  const opacity = 1 - Math.min(scrollY / heroHeight, 1);
-  const translateY = Math.min(scrollY / 5, 60);
+document.addEventListener('DOMContentLoaded', () => {
+  const heroSection = document.getElementById('heroSection');
+  const backToTopButton = document.querySelector('.back-to-top');
 
-  heroSection.style.opacity = opacity;
-  heroSection.style.transform = `translateY(-${translateY}px)`;
+  window.addEventListener('scroll', () => {
+    // Hero section parallax effect
+    const scrollY = window.scrollY;
+    const heroHeight = heroSection.offsetHeight;
+    const opacity = 1 - Math.min(scrollY / heroHeight, 1);
+    const translateY = Math.min(scrollY / 5, 60);
+    heroSection.style.opacity = opacity;
+    heroSection.style.transform = `translateY(-${translateY}px)`;
+
+    // Back to top button visibility
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 });
